3411f30a5dc46061d0ab0f0184d7a45282236955
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-04-04 15:32:48 -0700

    Add files via upload

---
+++ FirstJSProject/JavaScript.js
@@ -0,0 +1,19 @@
+//this is a comment in JS
+//this is JS zone
+//alert("hello world");
+//prompt("Enter your name");
+console.log("hello world");
+console.warn("This is a warning");
+console.error("Fatal Error ... buy another computer");
+
+var user; // intialization
+user="Samantha";//value assignation
+
+var age=99
+
+let studentName="Astrid";
+const taxes=0.08;
+console.log(100*taxes);
+
+document.write("<h6>Hello " + studentName + "</h6>");
+document.write(`<h6>Hello ${studentName} </h6>`);
