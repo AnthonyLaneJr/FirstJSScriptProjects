f740a2fdf723514ad6200d71009c163770c10d07
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-04-04 22:46:23 -0700

    Assignment #1
    Module 103 Assignment # 1

--- FirstJSProject/JavaScript.js
+++ FirstJSProject/JavaScript.js
@@ -1,19 +1,74 @@
-//this is a comment in JS
-//this is JS zone
-//alert("hello world");
-//prompt("Enter your name");
-console.log("hello world");
-console.warn("This is a warning");
-console.error("Fatal Error ... buy another computer");
+let Website = "Netflix"
+let Language = "English"
+let SignIn = "Sign In"
+let MainTitle = "Unlimited movies, TV shows, and more."
+let SubTitle = "Watch anywhere. Cancel Anytime"
+let SubSubTitle = "Ready to watch? Enter your email to create or restart your membership."
+let Email = "Email@email.com"
+let Password = "Password"
+let Login = "Get Started"
+let AdTitle1 = "Enjoy on your TV."
+let AdText1 = "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+let AdTitle2 = "Download your shows to watch offline."
+let AdText2 = "Save your favorites easily and always have something to watch."
+let AdTitle3 = "Watch everywhere."
+let AdText3 = "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more."
+let AdTitle4 = "Create profiles for kids."
+let AdText4 = "Send kids on adventures with their favorite characters in a space made just for them-free with your membership."
+let Questions = "Questions?"
+let Contact =  "Call 1-844-505-2993"
+let BottomLanguage = "English"
 
-var user; // intialization
-user="Samantha";//value assignation
 
-var age=99
-
-let studentName="Astrid";
-const taxes=0.08;
-console.log(100*taxes);
-
-document.write("<h6>Hello " + studentName + "</h6>");
-document.write(`<h6>Hello ${studentName} </h6>`);
+document.write(
+    `<div>
+    <h2>JavaScript Insertions</h2>
+    1. <p class=website>${Website}</p>
+    <br>
+    2. <p class=lang>${Language}</p>
+    <br>
+    3. ${SignIn}
+    <br> 
+    <br>
+    4. <p class=MainTitle>${MainTitle}</p>
+    <br>
+    5. <p class=SubTitle>${SubTitle}</p>
+    <br> 
+    6. <p class=SubTitle>${SubSubTitle}</p>
+    <br>
+    <br>
+    7. ${Email}
+    <br> 
+    8. ${Password}
+    <br>
+    9. ${Login}
+    <br> 
+    <br>
+    10. <p class=SubTitle>${AdTitle1}</p>
+    <br>
+    11. ${AdText1}
+    <br> 
+    <br>
+    12. <p class=SubTitle>${AdTitle2}</p>
+    <br>
+    13. ${AdText2}
+    <br> 
+    <br>
+    14. <p class=SubTitle>${AdTitle3}</p>
+    <br>
+    15. ${AdText3}
+    <br> 
+    <br>
+    16. <p class=SubTitle>${AdTitle4}</p>
+    <br>
+    17. ${AdText4}
+    <br> 
+    <br>
+    <br>
+    18. ${Questions}
+    <br> 
+    19. ${Contact}
+    <br>
+    20. <p class=lang>${BottomLanguage}</p>
+    </div>`
+    )
